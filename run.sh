102cf944adb871f0c5824f3fa1afc56052f6c1c7
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-04-14 00:38:10 +0530

    Added run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+mkdir Screenshots
+python3 screenshot.py
+cd Screenshots
+ffmpeg -framerate 5 -i Screenshot%01d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p output.mp4
+cd ..
+chmod +x move.sh
+./move.sh
